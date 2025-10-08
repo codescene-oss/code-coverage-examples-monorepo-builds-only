4719e2d30a0ea9409a658fe5a2ba853d7446f036
Juraj Martinka <jumarko@gmail.com>
2025-10-08 07:04:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CodeScene Code Coverage examples - monorepo - builds only (no PRs)
+
+This demonstrates CI build pipeline configuration in the "monorepo" setting,
+where we only run "builds" outside of any PR context.
+
+
+In .github/workflows, you will find corresponding build pipelines definitions.
---
+++ calculator-1/src/main/java/com/codescene/calculator1/ConditionalExample.java
@@ -0,0 +1,64 @@
+package com.codescene.calculator1;
+
+public class ConditionalExample {
+
+    /**
+     * This is an example on documentation. Modifying docs should never impact 
+     * coverage: docs are non-executable lines.
+     * @param num (a great name for an argument, isn't it?)
+     * @return Nothing. This method is all about side-effects.
+     */
+    public void checkNumber(int num) {
+        if (num > 0) {
+            // Let's add a comment inside the method body.
+            // We'll use this comment block as a modification target in 
+            // various PRs to illustrate how comments are filtered out from 
+            // the coverage checks.
+            System.out.println("The number is positive");
+        } else if (num < 0) {
+            System.out.println("The number is negative");
+        }
+
+        if (num % 2 == 0) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+
+    public String categorizeString(String str) {
+        String result = "";
+
+        if (str.length() > 5) {
+            result += "Long ";
+        } else {
+            result += "Short ";
+        }
+
+        if (str.matches(".*\\d.*")) {
+            result += "and contains numbers";
+        } else {
+            result += "and contains only letters";
+        }
+
+        return result;
+    }
+
+    public int countVowels(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (vowels.indexOf(ch) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
